4d9a329b26d16ef6829ead9e000b7615408e9b7a
criskell <cris.moraes.ds@gmail.com>
2025-04-13 06:32:03 -0300

    test: add pest testing framework

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\API\CampaignController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum', function (Request $request) {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     })->middleware('auth:sanctum');
