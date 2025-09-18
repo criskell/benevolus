20d9f2784ed72d64c7f05a11177124fef6b88199
criskell <cris.moraes.ds@gmail.com>
2025-09-18 05:04:43 -0300

    feat(web): make hero section component

---
+++ web/src/app/(landing)/hero-section.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { Button } from '@heroui/react';
+import { Icon } from '@iconify/react';
+
+export function HeroSection() {
+  return (
+    <section className="z-20 flex flex-col items-center justify-center gap-[18px] sm:gap-6">
+      <Button
+        className="border-default-100 bg-default-50 text-small text-default-500 h-9 overflow-hidden border-1 px-[18px] py-2 leading-5 font-normal"
+        endContent={
+          <Icon
+            className="flex-none outline-hidden [&>path]:stroke-2"
+            icon="solar:arrow-right-linear"
+            width={20}
+          />
+        }
+        radius="full"
+        variant="bordered"
+      >
+        Novas mudanças
+      </Button>
+      <div className="text-center text-[clamp(40px,10vw,40px)] leading-[1.2] font-bold tracking-tighter sm:text-[46px]">
+        <div>
+          A maneira mais
+          <br /> fácil de fazer a diferença.
+        </div>
+      </div>
+      <p className="text-default-500 text-center leading-7 font-normal sm:w-[466px] sm:text-[18px]">
+        Cada contribuição aproxima sonhos e ajuda quem mais precisa. Junte-se a
+        uma comunidade que faz a diferença.
+      </p>
+      <div className="flex flex-col items-center justify-center gap-6 sm:flex-row">
+        <Button color="primary" radius="full">
+          Faça uma doação
+        </Button>
+        <Button
+          className="border-default-100"
+          endContent={
+            <span className="bg-default-100 pointer-events-none flex h-[22px] w-[22px] items-center justify-center rounded-full">
+              <Icon
+                className="text-default-500 [&>path]:stroke-[1.5]"
+                icon="solar:arrow-right-linear"
+                width={16}
+              />
+            </span>
+          }
+          radius="full"
+          variant="bordered"
+        >
+          Crie uma nova campanha
+        </Button>
+      </div>
+    </section>
+  );
+}
--- web/src/app/(landing)/page.tsx
+++ web/src/app/(landing)/page.tsx
@@ -1,3 +1,11 @@
+'use client';
+
+import { HeroSection } from './hero-section';
+
 export default function Home() {
-  return <></>;
+  return (
+    <main className="container mx-auto flex flex-1 flex-col items-center justify-center overflow-hidden px-8 my-16">
+      <HeroSection />
+    </main>
+  );
 }
