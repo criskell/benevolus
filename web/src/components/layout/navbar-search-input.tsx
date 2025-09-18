dfdd83696813bbba2a94679f567ad73c10527645
criskell <cris.moraes.ds@gmail.com>
2025-09-18 04:35:29 -0300

    feat(web): add heroui navbar

---
+++ web/src/components/layout/navbar-search-input.tsx
@@ -0,0 +1,25 @@
+import { Input } from '@heroui/input';
+import { Kbd } from '@heroui/kbd';
+
+import { SearchIcon } from '@/components/icons/search';
+
+export const navbarSearchInput = (
+  <Input
+    aria-label="Pesquisar campanhas..."
+    classNames={{
+      inputWrapper: 'bg-default-100',
+      input: 'text-sm',
+    }}
+    endContent={
+      <Kbd className="hidden lg:inline-block" keys={['command']}>
+        K
+      </Kbd>
+    }
+    labelPlacement="outside"
+    placeholder="Pesquisar campanhas..."
+    startContent={
+      <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
+    }
+    type="search"
+  />
+);
---
+++ web/src/config/site.ts
@@ -0,0 +1,28 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: 'Benevolus',
+  description:
+    'Una forças e ajude quem mais precisa. Cada gota faz a chuva acontecer.',
+  navItems: [
+    {
+      label: 'Início',
+      href: '/',
+    },
+    {
+      label: 'Campanhas',
+      href: '/campaigns',
+    },
+    {
+      label: 'Sobre nós',
+      href: '/about-us',
+    },
+    {
+      label: 'Dúvidas?',
+      href: '/faq',
+    },
+  ],
+  navMenuItems: [] as {
+    label: string;
+  }[],
+};
