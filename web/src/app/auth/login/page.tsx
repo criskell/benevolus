670d59e1e02428f841e851b0f382b5189bc97861
Ranskyth <ga.lima2205@gmail.com>
2025-12-15 23:16:16 -0300

    refactor: page login

---
+++ web/src/app/auth/login/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import React, { FormEvent } from "react";
+import {
+  AuthCard,
+  AuthInput,
+  AuthButton,
+  AuthForm,
+  AuthLink,
+} from "@/app/auth/_components";
+
+export default function App() {
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const inputs = new FormData(e.currentTarget);
+
+    const payload = {
+      email: inputs.get("email"),
+      password: inputs.get("password"),
+    };
+
+    console.log(payload);
+  };
+
+  return (
+    <AuthCard title="Login">
+      <AuthForm onSubmit={handleSubmit}>
+        <AuthInput
+          label="Email"
+          name="email"
+          placeholder="seu@email.com"
+          type="email"
+        />
+        <AuthInput
+          label="Senha"
+          name="password"
+          placeholder="Digite sua senha"
+          type="password"
+        />
+        <div className="flex flex-col gap-2">
+          <AuthLink text="Não tem conta?" linkText="Criar conta" href="/auth/sign-up" />
+          <p className="text-center text-small">
+            Esqueceu a senha? <a href="/auth/reset-password" className="text-primary">Recuperar aqui</a>
+          </p>
+        </div>
+        <div className="flex gap-2 justify-end">
+          <AuthButton>Entrar</AuthButton>
+        </div>
+      </AuthForm>
+    </AuthCard>
+  );
+}
