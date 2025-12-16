6ca9076a084285c630117931dc7a47feb7876166
Ranskyth <ga.lima2205@gmail.com>
2025-12-15 23:18:22 -0300

    feat: created export componets

---
+++ web/src/app/auth/_components/index.ts
@@ -0,0 +1,5 @@
+export { AuthCard } from "./AuthCard";
+export { AuthInput } from "./AuthInput";
+export { AuthButton } from "./AuthButton";
+export { AuthForm } from "./AuthForm";
+export { AuthLink } from "./AuthLink";
